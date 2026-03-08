ef209cb91d27ab4a1c192d6277de8561eb60e962
maarten-mdt <maarten-mdt@users.noreply.github.com>
2026-03-07 16:28:51 -0800

    Update

--- server/src/index.js
+++ server/src/index.js
@@ -43,6 +43,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ success: false, error: err.message ?? 'Internal server error' })
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`TacticalShack API listening on port ${PORT}`)
 })
